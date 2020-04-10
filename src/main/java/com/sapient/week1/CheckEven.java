487f0c491c4c0a256052595e67e4b44457f7e9ab
muskan <mbnm2004@gmail.com>
2020-04-10 14:08:32 +0530

    creating checkeven class

---
+++ src/main/java/com/sapient/week1/CheckEven.java
@@ -0,0 +1,15 @@
+package com.sapient.week1;
+
+public class CheckEven 
+{
+    public static void main( String[] args )
+    {
+     	check(5);
+	check(21);
+	check(98);   
+    }
+    public boolean check(int n)
+    {
+	return n%2;
+    }
+}
